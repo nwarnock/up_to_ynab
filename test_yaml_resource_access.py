9c1cae49a100a4d15e57bd93728a0393e8081e76
Nick Warnock <nickwarnock01@gmail.com>
2025-04-12 23:43:20 +0930

    Add script investigating use of yaml to store static account conversion info

---
+++ test_yaml_resource_access.py
@@ -0,0 +1,34 @@
+import os
+import yaml
+
+"""
+Testing how to access resources stored in a yaml
+Will use to link static account information between Up and YNAB
+"""
+
+# Load resources.yaml
+with open("resources.yaml", "r") as file:
+    resources = yaml.safe_load(file)
+
+accounts = resources['accounts']
+# print(accounts)
+# print(type(accounts))
+
+# print(accounts[0])
+
+for account in accounts:
+    up_id = account['up']['id']
+    ynab_id = account['ynab']['id']
+    print(f"Up ID: {up_id}; YNAB ID: {ynab_id}")
+
+# How can I use it to find a YNAB account id, given an Up account id?
+query = accounts[0]['up']['id']
+for account in accounts:
+    if(account['up']['id'] == query):
+        budget_id = account['ynab']['id']
+        break
+
+if budget_id:
+    print(f"The YNAB id for Up account {query} is: {budget_id}")
+else:
+    print(f"No matching YNAB account found for Up account {query}")
